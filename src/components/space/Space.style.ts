249922f2567f71274636963eecf25bfbb8d38b47
Haja <hajarakotozafy45@gmail.com>
2023-05-27 20:23:10 +0300

    refactor: cleaning project

--- src/components/space/Space.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import {ISpace} from "./Space";
-
-const SpaceStyled = styled.div<ISpace>`
-    width: ${props => props.theme.size(props.size)}px;
-    height: 100%;
-`
-
-export default SpaceStyled
