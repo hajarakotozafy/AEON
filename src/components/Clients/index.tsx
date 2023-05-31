beafa2da8ee6911a246b5b911a6b2975440c09c3
Haja <hajarakotozafy45@gmail.com>
2023-05-31 19:55:09 +0300

    fix: fix clients logo slider

--- src/components/Clients/index.tsx
+++ src/components/Clients/index.tsx
@@ -3,49 +3,29 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
-import Logo1 from "../../assets/images/logo1.svg"
-import Logo2 from "../../assets/images/logo2.svg"
-import Logo3 from "../../assets/images/logo3.svg"
-import Logo4 from "../../assets/images/logo4.svg"
-import Logo6 from "../../assets/images/logo6.svg"
+import Logo1 from "../../assets/images/client_logo_1.svg"
+import Logo2 from "../../assets/images/client_logo_2.svg"
+import Logo3 from "../../assets/images/client_logo_3.svg"
+import Logo4 from "../../assets/images/client_logo_4.svg"
+import Logo5 from "../../assets/images/client_logo_5.svg"
+import Logo6 from "../../assets/images/client_logo_6.svg"
+import Logo7 from "../../assets/images/client_logo_7.svg"
+import Logo8 from "../../assets/images/client_logo_8.svg"
 import { Img, Slide, SliderLogo, SliderTitle } from './Clients.style';
 
 
 const Clients: React.FC = () => {
-    var settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: false
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              initialSlide: 2
-            }
-          }
-          // {
-          //   breakpoint: 480,
-          //   settings: {
-          //     slidesToShow: 1,
-          //     slidesToScroll: 1
-          //   }
-          // }
-        ]
-      };
+  const settings = {
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    cssEase: "linear"
+  };
     return (
         <div id="clients">
         <SliderTitle>
@@ -66,22 +46,16 @@ const Clients: React.FC = () => {
             <Img src={Logo4} alt=""/>
           </div>
           <div>
-            <Img src={Logo6} alt=""/>
-          </div>
-          <div>
-            <Img src={Logo1} alt=""/>
-          </div>
-          <div>
-            <Img src={Logo2} alt=""/>
-          </div>
-          <div>
-            <Img src={Logo3} alt=""/>
-          </div>
-          <div>
-            <Img src={Logo4} alt=""/>
+            <Img src={Logo5} alt=""/>
           </div>
           <div>
             <Img src={Logo6} alt=""/>
+          </div>
+          <div>
+            <Img src={Logo7} alt=""/>
+          </div>
+          <div>
+            <Img src={Logo8} alt=""/>
           </div>
         </Slider>
         </SliderLogo>
